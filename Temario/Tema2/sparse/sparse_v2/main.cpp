9366e89980fd577d2ceaad1b6cea60614a64f04a
Jorge Riera-Ledesma <jriera@ull.edu.es>
2024-02-21 10:59:32 +0000

    mejoras en vectores dispersos

--- Temario/Tema2/sparse/sparse_v2/main.cpp
+++ Temario/Tema2/sparse/sparse_v2/main.cpp
@@ -3,42 +3,68 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <cstdio>
+#include <ctime>
 
 #define V_SZ 10000
+#define NZ 8
 #define EPS  1E-3
 
 int main(void)
 {
-	AyED::vector<double> v(V_SZ);
-
-	for(int i = 0; i < V_SZ; i++)
-		v.get_set_v(i) = 0.0;
-
-	v[0]    = 1.0; 
-	v[1000] = 1.0; 
-	v[2000] = 1.0; 
-	v[3000] = 1.0; 
-	v[4000] = 1.0; 
-	v[5000] = 1.0;
-
-	AyED::sparse_vector sv(v, EPS);	
-
-	std::cout << sv << std::endl;
-
-	// -------------------------------------
+	srand (time(NULL));
 
 	AyED::vector<double> v1(V_SZ);
 
-	// Rellena v1 con valores aleatorios
+	for(int i = 0; i < V_SZ; i++)
+		v1[i] = 0.0;
+
+	for(int i = 0; i < NZ; i++)
+		v1[rand() % V_SZ] = (rand() % 1000) / 100.0;
+
+	AyED::sparse_vector sv1(v1, EPS);	
+
+	std::cout << "Muestra vector disperso sv1" << std::endl;
+	std::cout << std::endl;
+	std::cout << sv1 << std::endl;
+
+	AyED::vector<double> v2(V_SZ);
 
 	for(int i = 0; i < V_SZ; i++)
-		v1.get_set_v(i) = (rand() % 1000) / 100.0;
+		v2[i] = 0.0;
 
+	for(int i = 0; i < NZ; i++)
+		v2[rand() % V_SZ] = (rand() % 1000) / 100.0;	
 
-	const double producto_1 = sv.scal_prod(v1);
-	const double producto_2 = v1 * sv;
-	const double producot_3 = sv * v1;
+	AyED::sparse_vector sv2(v2, EPS);
 
+	std::cout << "Muestra vector disperso sv2" << std::endl;
+	std::cout << std::endl;
+	std::cout << sv2 << std::endl;
 
-	
+	const double producto_1 = v1 * sv1;
+	const double producto_2 = sv1 * v1;
+
+	const double producto_3 = sv2 * v2;
+	const double producto_4 = v2 * sv2;
+
+	const double producto_5 = sv1 * sv2;
+	const double producto_6 = sv1 * sv1;
+	const double producto_7 = sv2 * sv2;
+
+	std::cout << "Muestra productos escalares" << std::endl;
+	std::cout << std::endl;
+	std::cout << std::setw(8) << std::fixed << std::setprecision(1) << producto_1 << std::endl;
+	std::cout << std::setw(8) << std::fixed << producto_2 << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << std::setw(8) << std::fixed << producto_3 << std::endl;
+	std::cout << std::setw(8) << std::fixed << producto_4 << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << std::setw(8) << std::fixed << producto_5 << std::endl;
+	std::cout << std::setw(8) << std::fixed << producto_6 << std::endl;
+	std::cout << std::setw(8) << std::fixed << producto_7 << std::endl;
 }
