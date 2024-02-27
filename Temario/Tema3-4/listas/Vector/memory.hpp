34d32fc60af3aaadbaa2fefc2103cec0980f9387
Jorge Riera-Ledesma <jriera@ull.es>
2024-02-27 17:22:12 +0000

    Refinamiento de dos clases

--- Temario/Tema3-4/listas/Vector/memory.hpp
+++ Temario/Tema3-4/listas/Vector/memory.hpp
@@ -6,51 +6,67 @@
 #define MAX_SZ 100
 
 #define FREE -2
+#define NULL_PTR -1 // Asumiendo que NULL_PTR está definido como -1
 
-namespace AyED {
-    
+namespace AyED
+{
     template <class T>
-    class memory 
+    class memory
     {
-        private:
-            vector<AyED::sll_node<T> > mem_;
-            
-        public:
-            memory(int max_sz = MAX_SZ):
-            mem_(max_sz)
-            {
-                for(int i = 0; i < mem_.size(); i++)
-                    mem_[i].set_next(FREE);
-            }
-            
-            int New(const T& data) {
-                
-                int i;
-                
-                for(i = 0; (i < mem_.size()) && (mem_[i].get_next() != FREE); i++);
-                
-                if (i < mem_.size()) {
-                    mem_[i].set_data(data);
-                    mem_[i].set_next(NULL_PTR);
-                
-                    return i;
-                } else return NULL_PTR;
-            }
-            
-            void Delete(int inx) {
-                
-                mem_[inx].set_next(FREE);
-            }
-            
-            sll_node<T>& operator[](int i)
-            {
-                return mem_[i];
-            }
-            
-            const sll_node<T>& operator[](int i) const
-            {
-                return mem_[i];
-            }
+    public:
+        memory(int max_sz = MAX_SZ);
+
+        int New(const T &data);
+        void Delete(int inx);
+
+        sll_node<T> &operator[](int i);
+        const sll_node<T> &operator[](int i) const;
+
+    private:
+        vector<AyED::sll_node<T>> mem_;
     };
-    
+}
+
+// Implementaciones
+namespace AyED
+{
+    template <class T>
+    memory<T>::memory(int max_sz) : mem_(max_sz)
+    {
+        for (int i = 0; i < mem_.size(); i++)
+            mem_[i].set_next(FREE);
+    }
+
+    template <class T>
+    int memory<T>::New(const T &data)
+    {
+        int i;
+        for (i = 0; (i < mem_.size()) && (mem_[i].get_next() != FREE); i++);
+        if (i < mem_.size())
+        {
+            mem_[i].set_data(data);
+            mem_[i].set_next(NULL_PTR);
+            return i;
+        }
+        else
+            return NULL_PTR;
+    }
+
+    template <class T>
+    void memory<T>::Delete(int inx)
+    {
+        mem_[inx].set_next(FREE);
+    }
+
+    template <class T>
+    sll_node<T> &memory<T>::operator[](int i)
+    {
+        return mem_[i];
+    }
+
+    template <class T>
+    const sll_node<T> &memory<T>::operator[](int i) const
+    {
+        return mem_[i];
+    }
 }
--- Temario/Tema3-4/listas/Vector/vector.hpp
+++ Temario/Tema3-4/listas/Vector/vector.hpp
@@ -5,101 +5,124 @@
 
 namespace AyED
 {
+    template <class T>
+    class vector
+    {
+    public:
+        vector(void);
+        vector(int sz);
+        vector(const vector &v);
+        vector &operator=(const vector &v);
+        ~vector(void);
 
-	template <class T>
-	class vector
-	{
-	public:
-		vector(void) : v_(NULL),
-					   sz_(0) {}
+        void resize(int sz);
+        int size(void) const;
+        const T &at(int pos) const;
+        T &at(int pos);
 
-		vector(int sz) : v_(NULL),
-						 sz_(sz)
-		{
+        const T &operator[](int pos) const;
+        T &operator[](int pos);
 
-			crea_vector();
-		}
+    private:
+        void crea_vector(void);
+        void destruye_vector(void);
 
-		vector(const vector &v) : v_(NULL),
-								  sz_(v.sz_)
-		{
-			crea_vector();
+    private:
+        T *v_;
+        int sz_;
+    };
+}
 
-			for (int i = 0; i < sz_; i++)
-				v_[i] = v.v_[i];
-		}
+namespace AyED
+{
+    template <class T>
+    vector<T>::vector(void) : v_(NULL), sz_(0) {}
 
-		vector &operator=(const vector &v)
-		{
-			resize(v.sz_);
+    template <class T>
+    vector<T>::vector(int sz) : v_(NULL), sz_(sz)
+    {
+        crea_vector();
+    }
 
-			for (int i = 0; i < sz_; i++)
-				v_[i] = v.v_[i];
+    template <class T>
+    vector<T>::vector(const vector<T> &v) : v_(NULL), sz_(v.sz_)
+    {
+        crea_vector();
+        for (int i = 0; i < sz_; i++)
+            v_[i] = v.v_[i];
+    }
 
-			return *this;
-		}
+    template <class T>
+    vector<T> &vector<T>::operator=(const vector<T> &v)
+    {
+        if (this != &v)
+        {
+            resize(v.sz_);
+            for (int i = 0; i < sz_; i++)
+                v_[i] = v.v_[i];
+        }
+        return *this;
+    }
 
-		~vector(void)
-		{
+    template <class T>
+    vector<T>::~vector(void)
+    {
+        destruye_vector();
+    }
 
-			destruye_vector();
-		}
+    template <class T>
+    void vector<T>::resize(int sz)
+    {
+        destruye_vector();
+        sz_ = sz;
+        crea_vector();
+    }
 
-		void resize(int sz)
-		{
-			destruye_vector();
-			sz_ = sz;
-			crea_vector();
-		}
+    template <class T>
+    int vector<T>::size(void) const
+    {
+        return sz_;
+    }
 
-		int size(void) const
-		{
-			return sz_;
-		}
+    template <class T>
+    const T &vector<T>::at(int pos) const
+    {
+        assert((pos < sz_) && (pos >= 0));
+        return v_[pos];
+    }
 
-		const T &at(int pos) const
-		{
+    template <class T>
+    T &vector<T>::at(int pos)
+    {
+        assert((pos < sz_) && (pos >= 0));
+        return v_[pos];
+    }
 
-			assert((pos < sz_) && ((pos >= 0)));
+    template <class T>
+    const T &vector<T>::operator[](int pos) const
+    {
+        return at(pos);
+    }
 
-			return v_[pos];
-		}
+    template <class T>
+    T &vector<T>::operator[](int pos)
+    {
+        return at(pos);
+    }
 
-		T &at(int pos)
-		{
+    template <class T>
+    void vector<T>::crea_vector(void)
+    {
+        v_ = new T[sz_];
+    }
 
-			assert((pos < sz_) && ((pos >= 0)));
-
-			return v_[pos];
-		}
-
-		const T &operator[](int pos) const
-		{
-			return at(pos);
-		}
-
-		T &operator[](int pos)
-		{
-			return at(pos);
-		}
-
-	private:
-		void crea_vector(void)
-		{
-			v_ = new T[sz_];
-		}
-
-		void destruye_vector(void)
-		{
-			if (v_ != NULL)
-			{
-				delete[] v_;
-				v_ = NULL;
-			}
-		}
-
-	private:
-		T *v_;
-		int sz_;
-	};
+    template <class T>
+    void vector<T>::destruye_vector(void)
+    {
+        if (v_ != NULL)
+        {
+            delete[] v_;
+            v_ = NULL;
+        }
+    }
 }
